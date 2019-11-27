59954da7120d5228e67a3f49f97dae5a012783fd
shervin zadsoroor <shervinzadsoroor@gmail.com>
2019-11-27 17:26:00 +0330

    completed project as version one

--- src/com/shervinZadsoroor/tizpa/Main.java
+++ src/com/shervinZadsoroor/tizpa/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.printf("your trip price is : %.2f  toomans!", Payment.calculatePrice());
     }
 }
 

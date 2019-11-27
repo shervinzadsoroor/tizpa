b6dafdf88c6205e3efee9002a4db64a623ab8af6
shervin zadsoroor <shervinzadsoroor@gmail.com>
2019-11-27 16:48:30 +0330

    just modified economic class

--- src/com/shervinZadsoroor/tizpa/economy.java
+++ src/com/shervinZadsoroor/tizpa/economy.java
@@ -1,27 +1,27 @@
-package Tizpa;
+package com.shervinZadsoroor.tizpa;
 
 import java.util.Scanner;
 
-class economy extends Trip {
+public class economy extends Trip {
 
     public economy() {
         super.basePrice = 5000;
-        super.rainyAndTickOverBrake = 1.4;
+        super.rainyAndPickHourRate = 1.4;
+        super.pickHourRate = 1.2;
         super.rainyRate = 1.2;
-        super.pickOverRate = 1.2;
     }
 
-    public double economyPrice(){
+    public double economyPrice() {
         Scanner input = new Scanner(System.in);
         double rate = 0;
         System.out.println("Rainy And Pick Over = 1  PickOverRate = 2   Rainy = 3\n Please Chose A Numbber");
         int choice = input.nextInt();
         switch (choice) {
             case 1:
-                rate = basePrice * rainyAndTickOverBrake;
+                rate = basePrice * rainyAndPickHourRate;
                 break;
             case 2:
-                rate = basePrice * pickOverRate;
+                rate = basePrice * pickHourRate;
                 break;
             case 3:
                 rate = basePrice * rainyRate;
@@ -31,4 +31,26 @@ class economy extends Trip {
         return rate;
     }
 
+    @Override
+    protected double price() {
+        Scanner input = new Scanner(System.in);
+        double rate = 0.0;
+        System.out.println("(rainyAndPickHour = 1 , pickHour = 2 , rainy = 3)\n please choose your number: ");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                rate = basePrice * rainyAndPickHourRate;
+                break;
+            case 2:
+                rate = basePrice * pickHourRate;
+                break;
+            case 3:
+                rate = basePrice * rainyRate;
+                break;
+            default:
+                System.out.println("invalid choice!!!");
+                break;
+        }
+        return rate;
+    }
 }
